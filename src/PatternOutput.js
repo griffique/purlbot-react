35e3a7374828d800dd7312c88536cdfce0a8e592
Quinn <74791898+griffique@users.noreply.github.com>
2021-02-17 15:27:07 -0500

    Added link to yarn amount calculator and stitch suggestions

--- src/PatternOutput.js
+++ src/PatternOutput.js
@@ -24,6 +24,45 @@ export default function PatternOutput(props) {
           <li> Children's scarf measurement = (6" x 48")</li>{" "}
           <li> Adult scarf measurement = (8" x 60")</li>
         </ul>
+        <h4>Suggested Stitches</h4>
+        <ul>
+          <li>
+            <a
+              href="http://freeknitstitches.com/pattern.php?num=106&menu=2"
+              target="_blank"
+            >
+              Forked Lightning ⭐{" "}
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://freeknitstitches.com/pattern.php?num=216&menu=1"
+              target="_blank"
+            >
+              Twisted Rib ⭐⭐
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://freeknitstitches.com/pattern.php?num=290&menu=4"
+              target="_blank"
+            >
+              Sine Wave ⭐⭐⭐
+            </a>
+          </li>
+        </ul>{" "}
+        <ul id="difficulty-ratings">
+          <li>⭐ Easier </li>
+          <li>⭐⭐ Intermediate </li>
+          <li>⭐⭐⭐ Advanced</li>
+        </ul>
+        <h3>
+          {" "}
+          Not sure if you have enough yarn for your project?{" "}
+          <a href="https://www.JimmyBeansWool.com/knittingCalculator">
+            Find out here.
+          </a>
+        </h3>
       </div>
     );
   else {
