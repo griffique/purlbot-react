8c344f40f52f72ca0452cfab6dd6ef7edbe25fbd
Quinn <74791898+griffique@users.noreply.github.com>
2021-02-17 15:12:27 -0500

    Added scarf pattern calculation

--- src/PatternOutput.js
+++ src/PatternOutput.js
@@ -2,13 +2,26 @@ import React from "react";
 export default function PatternOutput(props) {
   if (props.project === "scarf")
     return (
-      <h1>
-        Building a {props.project} for {props.name} with a gauge of{" "}
-        {props.gauge} st/inch!
-      </h1>
+      <div className="patternDisplay">
+        <h1>
+          {props.name}'s {props.project} Pattern
+        </h1>
+        <h3>Sizes are given as (child, adult). </h3>
+        <p>
+          Cast on ({props.gauge * 6}, {props.gauge * 8}) stitches. Knit in your
+          chosen stitch until you measure (48, 60) inches from cast on, or
+          desired length, and bind off in pattern.{" "}
+          <a
+            href="https://blog.knitpicks.com/tutorial-blocking-knits/"
+            target="_blank"
+          >
+            Block{" "}
+          </a>
+          to measurements listed below.{" "}
+        </p>
+      </div>
     );
-  else
-    return (
-      <h1>"Mitten and Hat Functionality Coming Soon! Check back later.</h1>
-    );
+  else {
+    return <h1>Hat and Mitten Functionality Coming Soon. Check Back Later!</h1>;
+  }
 }
