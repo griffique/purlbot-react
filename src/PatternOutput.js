a9f8d4f67dd6c59c2c5dc4b2611e420b23b2f181
Quinn <74791898+griffique@users.noreply.github.com>
2021-02-17 14:57:32 -0500

    Added mitten and hat coming soon message to pattern output

--- src/PatternOutput.js
+++ src/PatternOutput.js
@@ -1,9 +1,14 @@
 import React from "react";
 export default function PatternOutput(props) {
-  return (
-    <h1>
-      Building a {props.project} for {props.name} with a gauge of {props.gauge}{" "}
-      st/inch!
-    </h1>
-  );
+  if (props.project === "scarf")
+    return (
+      <h1>
+        Building a {props.project} for {props.name} with a gauge of{" "}
+        {props.gauge} st/inch!
+      </h1>
+    );
+  else
+    return (
+      <h1>"Mitten and Hat Functionality Coming Soon! Check back later.</h1>
+    );
 }
