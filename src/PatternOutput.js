9b59b0f1ed8931f6563b9cc14d187d69ba16f84c
Quinn <74791898+griffique@users.noreply.github.com>
2021-02-22 14:24:58 -0500

    Put pattern suggestions into cards with images

--- src/PatternOutput.js
+++ src/PatternOutput.js
@@ -1,11 +1,10 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 export default function PatternOutput(props) {
   if (props.project === "scarf")
     return (
-      <div className="patternDisplay">
-        <h1>
-          {props.name}'s {props.project} Pattern
-        </h1>
+      <div className="container">
+        <h1>{props.name}'s Scarf Pattern</h1>
         <h3>Sizes are given as (child, adult). </h3>
         <p>
           Cast on ({props.gauge * 6}, {props.gauge * 8}) stitches. Knit in your
@@ -25,32 +24,58 @@ export default function PatternOutput(props) {
           <li> Adult scarf measurement = (8" x 60")</li>
         </ul>
         <h4>Suggested Stitches</h4>
-        <ul>
-          <li>
+        <div className="card">
+          <img
+            className="card-img-top"
+            src="http://freeknitstitches.com/Images/106/106-big.jpg"
+            alt="Image of pattern"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Forked Lightning ⭐</h5>
+
             <a
               href="http://freeknitstitches.com/pattern.php?num=106&menu=2"
-              target="_blank"
+              className="btn btn-primary"
             >
-              Forked Lightning ⭐{" "}
+              Go to pattern
             </a>
-          </li>
-          <li>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="card-img-top"
+            src="http://freeknitstitches.com/Images/216/216-big.jpg"
+            alt="Image of pattern"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Twisted Rib ⭐⭐</h5>
+
             <a
               href="http://freeknitstitches.com/pattern.php?num=216&menu=1"
-              target="_blank"
+              className="btn btn-primary"
             >
-              Twisted Rib ⭐⭐
+              Go to pattern
             </a>
-          </li>
-          <li>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="card-img-top"
+            src="http://freeknitstitches.com/Images/290/290-big.jpg"
+            alt="Image of pattern"
+          />
+          <div className="card-body">
+            <h5 className="card-title"> Sine Wave ⭐⭐⭐</h5>
+
             <a
               href="http://freeknitstitches.com/pattern.php?num=290&menu=4"
-              target="_blank"
+              className="btn btn-primary"
             >
-              Sine Wave ⭐⭐⭐
+              Go to pattern
             </a>
-          </li>
-        </ul>{" "}
+          </div>
+        </div>
+
         <ul id="difficulty-ratings">
           <li>⭐ Easier </li>
           <li>⭐⭐ Intermediate </li>
