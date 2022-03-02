e4b73dd6044dbbcb858a8ac3f31f32f1fe530636
Quinn <74791898+griffique@users.noreply.github.com>
2022-03-01 19:03:45 -0500

    image changes

--- src/About/About.js
+++ src/About/About.js
@@ -4,38 +4,25 @@ export default function About() {
   return (
     <div className="about-purlbot" id="about">
       <img
-        src="/images/aboutpurlbotpic.png"
-        className="card-img-top"
-        alt="knitting supplies"
+        src="/images/about.jpg"
+        className="card-img-top img-fluid"
+        alt="About Purlbot"
       />
       <div>
-        <div
-          id="about-purlbot-text"
-          style={{
-            margin: "-500px 0 350px",
-            display: "flex",
-            justifyContent: "center",
-            padding: "20px",
-            fontSize: "200px",
-          }}
-        >
-          <span id="bot-title-lg">About</span>
-          <span id="purl-title-lg"> Purl</span>
-          <span id="bot-title-lg">Bot</span>
+        <div className="text-area">
+          <p>
+            It's fun to go into your stash of leftover yarn and wonder, "what
+            could I make with this?" Less fun is browsing through endless pages
+            of patterns trying to find something that works with your yarn
+            weight, gauge and amount.
+          </p>
+          <p>
+            That's where <span id="purl-title-mini">Purl</span>
+            <span id="bot-title-mini">Bot</span> is different. Just tell us a
+            little bit about your project and we'll generate a basic pattern for
+            you.
+          </p>
         </div>
-        <p className="card-text">
-          It's fun to go into your stash of leftover yarn and wonder, "what
-          could I make with this?" Less fun is browsing through endless pages of
-          patterns trying to find something that works with your yarn weight,
-          gauge and amount.
-        </p>
-        <p className="card-text">
-          That's where <span id="purl-title-mini">Purl</span>
-          <span id="bot-title-mini">Bot</span> is different. Just tell us a
-          little bit about your project and we'll generate a basic pattern for
-          you.
-        </p>
-        <br />
       </div>
     </div>
   );
--- src/HowTo/HowTo.js
+++ src/HowTo/HowTo.js
@@ -3,57 +3,38 @@ export default function HowTo() {
   return (
     <div className="using-purlbot" id="how-to">
       <img
-        src="/images/knitconfetti.png"
-        className="img-fluid"
-        alt="yarn stash"
+        src="/images/howto.jpg"
+        className="card-img-top img-fluid"
+        alt="How to use Purlbot"
       />
       <div>
-        <div
-          className="title"
-          style={{
-            margin: "-225px 0 100px",
-            display: "flex",
-            justifyContent: "center",
-            padding: "20px",
-            fontSize: "200px",
-          }}
-        >
-          <span id="bot-title-lg">How to Use </span>
-          <span id="purl-title-lg" style={{ marginLeft: "20px" }}>
-            {" "}
-            Purl
-          </span>
-          <span id="bot-title-lg">Bot</span>
+        <div className="text-area">
+          <ol>
+            <li>
+              Choose your yarn and determine approximately how many yards you
+              have.
+            </li>
+            <li>Check the yarn label for suggested needle size.</li>
+            <li>
+              Using the yarn and needles you've chosen, knit a gauge swatch of
+              at least 4" x 4" in the stitch you intend to use for your
+              project.*
+            </li>
+            <li>
+              Determine the number of stitches you knit in one inch, both
+              horizontally and vertically. This is your gauge.
+            </li>
+            <li>
+              Use PurlBot to see what you can make and generate a custom
+              pattern!
+            </li>
+          </ol>
+          <p className="note mt-3">
+            *This is important because the stitch may change your gauge. A cable
+            pattern, for example, will make it tighter, while a lace pattern
+            often makes it looser.
+          </p>
         </div>
-        <ol className="card-text">
-          <li>
-            Choose your yarn and determine approximately how many yards you
-            have.
-          </li>
-          <li>Check the yarn label for suggested needle size.</li>
-          <li>
-            Using the yarn and needles you've chosen, knit a gauge swatch of at
-            least 4" x 4" in the stitch you intend to use for your project.*
-          </li>
-          <li>
-            Determine the number of stitches you knit in one inch, both
-            horizontally and vertically. This is your gauge.
-          </li>
-          <li>
-            Use PurlBot to see what you can make and generate a custom pattern!
-          </li>
-        </ol>
-        <p className="note mt-3">
-          *This is important because the stitch may change your gauge. A cable
-          pattern, for example, will make it tighter, while a lace pattern often
-          makes it looser. For more on gauge, see{" "}
-          <a
-            href="https://www.purlsoho.com/create/2020/08/20/all-about-gauge/"
-            target="_blank"
-          >
-            this post by Purl Soho.
-          </a>
-        </p>
       </div>
     </div>
   );
